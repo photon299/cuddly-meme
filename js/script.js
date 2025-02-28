a50cd63056c1e62453110cf4bb4153f9263eb120
Alex <alex.real1850@gmail.com>
2025-03-01 00:16:47 +0200

    lesson_47 not finished

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-/* Задания на урок:
-
-*1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-*2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-*3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-*Реализовать только при помощи JS
-
-*4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-*Отсортировать их по алфавиту 
-
-*5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
 const movieDB = {
@@ -23,25 +9,39 @@ const movieDB = {
         "Скотт Пилигрим против...",
     ]
 };
-movieDB["movies"].sort();
 
 document.querySelectorAll(".promo__adv img").forEach(ad => ad.remove());
 document.querySelector(".promo__genre").innerText = "ДРАМА";
 document.querySelector(".promo__bg").style.background = `url("../img/bg.jpg") center center/cover no-repeat`;
 
-const movieList = document.querySelector(".promo__interactive-list");
-movieList.innerHTML = "";
-// for (let i = 0; i < movieDB["movies"].length; i++) {
-//     movieList.insertAdjacentHTML("beforeend", `
-//         <li class="promo__interactive-item">${i + 1} ${movieDB["movies"][i]}
-//             <div class="delete"></div>
-//         </li>
-//     `);
-// }
-movieDB.movies.forEach((m, i) => {
-    movieList.insertAdjacentHTML("beforeend", `
-        <li class="promo__interactive-item">${i + 1} ${m}
-            <div class="delete"></div>
-        </li>
-    `);
+const movieList = document.querySelector(".promo__interactive-list"),
+      input = document.querySelector(".adding__input"),
+      checkbox = document.querySelector("[type='checkbox']"),
+      btn = document.querySelector("button");
+let deleteBtns;
+const collectDeleteBtns = () => {
+    deleteBtns = document.querySelectorAll(".delete");
+}
+
+const renderMovieList = () => {
+    movieList.innerHTML = "";
+    movieDB["movies"].sort();
+    movieDB.movies.forEach((m, i) => {
+        movieList.insertAdjacentHTML("beforeend", `
+            <li class="promo__interactive-item">${i + 1} ${m}
+                <div class="delete"></div>
+            </li>
+        `);
+    });
+    collectDeleteBtns();
+};
+renderMovieList();
+
+btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    let movieName = (input.value.length > 21) ? input.value.slice(0, 21) + "..." : input.value;
+    if (checkbox.checked && input.value) console.log("Добавляем любимый фильм");
+    movieDB.movies.push(movieName);
+    renderMovieList();
+    input.value = "";
 });
