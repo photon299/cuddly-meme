e2b6130cbbb1eea0bec38f37b2a388c2c4e23f38
Alex <alex.real1850@gmail.com>
2025-03-01 13:40:50 +0200

    lesson_43 final commit

--- js/script.js
+++ js/script.js
@@ -1,47 +1,65 @@
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против...",
-    ]
-};
+document.addEventListener("DOMContentLoaded", () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против...",
+        ]
+    };
+    
+    const adverts = document.querySelectorAll(".promo__adv img"),
+          movieList = document.querySelector(".promo__interactive-list"),
+          input = document.querySelector(".adding__input"),
+          checkbox = document.querySelector("[type='checkbox']"),
+          addMovieBtn = document.querySelector("button");
+    
+    const deleteAdv = (adv) => adv.forEach(ad => ad.remove());
 
-document.querySelectorAll(".promo__adv img").forEach(ad => ad.remove());
-document.querySelector(".promo__genre").innerText = "ДРАМА";
-document.querySelector(".promo__bg").style.background = `url("../img/bg.jpg") center center/cover no-repeat`;
+    const sortArray = (arr) => arr.sort();
 
-const movieList = document.querySelector(".promo__interactive-list"),
-      input = document.querySelector(".adding__input"),
-      checkbox = document.querySelector("[type='checkbox']"),
-      btn = document.querySelector("button");
-let deleteBtns;
-const collectDeleteBtns = () => {
-    deleteBtns = document.querySelectorAll(".delete");
-}
+    const makeChanges = () => {
+        document.querySelector(".promo__genre").innerText = "ДРАМА";
+        document.querySelector(".promo__bg").style.background = `url("../img/bg.jpg") center center/cover no-repeat`;
+    }
 
-const renderMovieList = () => {
-    movieList.innerHTML = "";
-    movieDB["movies"].sort();
-    movieDB.movies.forEach((m, i) => {
-        movieList.insertAdjacentHTML("beforeend", `
-            <li class="promo__interactive-item">${i + 1} ${m}
-                <div class="delete"></div>
-            </li>
-        `);
+    const modifyDeleteBtns = (render) => {
+        document.querySelectorAll(".delete").forEach((button, i) => {
+            button.addEventListener("click", () => {
+                movieDB.movies.splice(i, 1);
+                render();
+            });
+        });
+    }
+
+    const renderMovieList = () => {
+        movieList.innerHTML = "";
+        sortArray(movieDB.movies);
+        movieDB.movies.forEach((movieName, i) => {
+            movieList.insertAdjacentHTML("beforeend", `
+                <li class="promo__interactive-item">${i + 1} ${movieName}
+                    <div class="delete"></div>
+                </li>
+            `);
+        });
+        modifyDeleteBtns(renderMovieList);
+    };
+    
+    addMovieBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        let movieName = (input.value.length > 21) ? `${input.value.slice(0, 21)}...` : input.value;
+        if (input.value) {
+            if (checkbox.checked) console.log("Добавляем любимый фильм");
+            movieDB.movies.push(movieName);
+            renderMovieList();
+            input.value = "";
+        }
     });
-    collectDeleteBtns();
-};
-renderMovieList();
 
-btn.addEventListener("click", (event) => {
-    event.preventDefault();
-    let movieName = (input.value.length > 21) ? input.value.slice(0, 21) + "..." : input.value;
-    if (checkbox.checked && input.value) console.log("Добавляем любимый фильм");
-    movieDB.movies.push(movieName);
+    deleteAdv(adverts);
+    makeChanges();
     renderMovieList();
-    input.value = "";
 });
